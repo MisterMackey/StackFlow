06d7d74d3eafb339cf83c73595ec6753fde2af64
Jasper Ris <jasper.ris@nl.abnamro.com>
2020-01-28 11:35:24 +0100

    progressed on user input form

--- StackFlow/SupportingForms/NewItemInputForm.Designer.cs
+++ StackFlow/SupportingForms/NewItemInputForm.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TextBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ListBoxPriority = new System.Windows.Forms.ListBox();
+            this.RichTextBoxDescription = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // TextBoxName
+            // 
+            this.TextBoxName.Location = new System.Drawing.Point(13, 38);
+            this.TextBoxName.Name = "TextBoxName";
+            this.TextBoxName.Size = new System.Drawing.Size(316, 23);
+            this.TextBoxName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Description";
+            // 
+            // ListBoxPriority
+            // 
+            this.ListBoxPriority.FormattingEnabled = true;
+            this.ListBoxPriority.ItemHeight = 15;
+            this.ListBoxPriority.Location = new System.Drawing.Point(352, 38);
+            this.ListBoxPriority.Name = "ListBoxPriority";
+            this.ListBoxPriority.Size = new System.Drawing.Size(80, 109);
+            this.ListBoxPriority.TabIndex = 3;
+            // 
+            // RichTextBoxDescription
+            // 
+            this.RichTextBoxDescription.Location = new System.Drawing.Point(13, 89);
+            this.RichTextBoxDescription.Name = "RichTextBoxDescription";
+            this.RichTextBoxDescription.Size = new System.Drawing.Size(316, 87);
+            this.RichTextBoxDescription.TabIndex = 4;
+            this.RichTextBoxDescription.Text = "";
+            // 
+            // NewItemInputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(444, 188);
+            this.Controls.Add(this.ListBoxPriority);
+            this.Controls.Add(this.RichTextBoxDescription);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxName);
+            this.Name = "NewItemInputForm";
             this.Text = "NewItemInputForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TextBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox ListBoxPriority;
+        private System.Windows.Forms.RichTextBox RichTextBoxDescription;
     }
 }
