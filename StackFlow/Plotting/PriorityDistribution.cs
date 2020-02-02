387fe2d59b553995911e3a43de240bff5f8419fb
Jasper Ris <jasperris93@gmail.com>
2020-02-02 17:15:15 +0100

    added some plot lib, gotta play around iwth it

---
+++ StackFlow/Plotting/PriorityDistribution.cs
@@ -0,0 +1,29 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackFlow.Plotting
+{
+    public class PriorityDistribution
+    {
+        public void init()
+        {
+            var testmodel = new PlotModel();
+            var series = new BarSeries();
+            var iti = new List<BarItem>();
+            BarItem itum = new BarItem(value: 2);
+            iti.Add(itum);
+            series.ItemsSource = iti;
+            testmodel.Series.Add(series);
+
+            PlotView view = new PlotView();
+            view.Model = testmodel;
+            //todo: draw a form with this and play around
+            
+        }
+        
+    }
+}
