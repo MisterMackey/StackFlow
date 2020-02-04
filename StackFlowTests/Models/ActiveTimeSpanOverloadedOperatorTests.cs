71469082c2f39bef4cc684d79c29889e79200bf2
Jasper Ris <jasper.ris@nl.abnamro.com>
2020-02-04 16:07:10 +0100

    added tests for ativetimespan struct implicit conversions and equals operator overload

---
+++ StackFlowTests/Models/ActiveTimeSpanOverloadedOperatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackFlow.Models.Tests
+{
+    [TestClass()]
+    public class ActiveTimeSpanOverloadedOperatorTests
+    {
+        [TestMethod()]
+        public void ActiveTimeSpanOperatorTests()
+        {
+            //implicit operator datetimeoffset to activetimespan
+            DateTimeOffset dto = DateTimeOffset.Now;
+            ActiveTimeSpan obj = dto; //conversion happens here
+            Assert.AreEqual(obj.ActivatedAbsoluteTime, dto);
+
+            //implicit operater activetimespan to timespan?
+            TimeSpan ts = new TimeSpan(1000);
+            obj.ActiveTime = ts;
+            TimeSpan? newtS = obj; //conversion happens here
+            Assert.AreEqual(newtS, ts);
+
+            //equals operator, it compares timespans
+            ActiveTimeSpan obj2 = new ActiveTimeSpan() { ActiveTime = new TimeSpan(2000) };
+            Assert.AreNotEqual(obj, obj2);
+            obj2.ActiveTime = new TimeSpan(1000);
+            Assert.AreEqual(obj, obj2);
+        }
+    }
+}
---
+++ StackFlowTests/Statistics/ValueTypesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackFlow.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackFlow.Statistics.Tests
+{
+    [TestClass()]
+    public class ValueTypesTests
+    {
+        [TestMethod()]
+        public void TransformToSessionStructTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TransformToStackStructTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TransformToItemStructTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TransformToActiveTimeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
