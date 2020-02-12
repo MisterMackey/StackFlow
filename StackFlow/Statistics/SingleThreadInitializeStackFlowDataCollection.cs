550d47db42323c8dbb796cedd3cafca7ac082c64
Jasper Ris <jasperris93@gmail.com>
2020-02-12 20:11:26 +0100

    slayed the monster using the power of refactor

--- StackFlow/Statistics/SingleThreadInitializeStackFlowDataCollection.cs
+++ StackFlow/Statistics/SingleThreadInitializeStackFlowDataCollection.cs
@@ -9,6 +9,7 @@ namespace StackFlow.Statistics
 {
     internal class SingleThreadInitializeStackFlowDataCollection : IStackFlowDataCollections
     {
+        #region private fields
         private IEnumerable<StackFlowSession> SourceData;
         private bool IsInitialized;
         private bool IsInitializing;
@@ -16,7 +17,9 @@ namespace StackFlow.Statistics
         private Stack[] _Stacks;
         private Item[] _Items;
         private ActiveTime[] _Times;
+        #endregion
 
+        #region Public
         public IEnumerable<Session> Sessions => TryGetSessions();
 
         public IEnumerable<Stack> Stacks => TryGetStacks();
@@ -47,94 +50,70 @@ namespace StackFlow.Statistics
             List<ActiveTime> tempTiems = new List<ActiveTime>(sescnt * 4000);
 
             //start initializing
-            int i=0 , j =0 , k =0, l = 0; //some iterators ima need
-            foreach (var source in SourceData)
-            {
-                tempSeshs.Add(source.TransformToSessionStruct(i));
-                //handle stacks in session
-                foreach (WorkStack innerSource in source.Session)
-                {
-                    tempStacks.Add(innerSource.TransformToStackStruct(i, j));
-                    //handle items in stack
-                    foreach (WorkStackItem innerInnerSource in innerSource)
-                    {
-                        tempItems.Add(innerInnerSource.TransformToItemStruct(i, j, k));
-                        //handle activetimes in item
-                        foreach (ActiveTimeSpan innerInnerInnerSource in innerInnerSource.PeriodsWhenActivated)
-                        {
-                            tempTiems.Add(innerInnerInnerSource.TransformToActiveTime(i, j, k, l));
-                            //we hit rock bottom
-                            l++;
-                        }
-                        k++;
-                    }
-                    //handle completed items in hte stack
-                    foreach (WorkStackItem innerInnerSource in innerSource.CompletedItems)
-                    {
-                        tempItems.Add(innerInnerSource.TransformToItemStruct(i, j, k));
-                        //handle activetimes in item
-                        foreach (ActiveTimeSpan innerInnerInnerSource in innerInnerSource.PeriodsWhenActivated)
-                        {
-                            tempTiems.Add(innerInnerInnerSource.TransformToActiveTime(i, j, k, l));
-                            //we hit rock bottom.. again
-                            l++;
-                        }
-                        k++;
-                    }
-                    //handle activetimespans in stack, these will get -1 as their itemId
-                    foreach (ActiveTimeSpan innerInnerSource in innerSource.PeriodsWhenActivated)
-                    {
-                        tempTiems.Add(innerInnerSource.TransformToActiveTime(i, j, -1, l));
-                        l++;
-                    }
-                    j++;
-                }
-                //handle completed stacks
-                foreach (WorkStack innerSource in source.CompletedStacks)
-                {
-                    tempStacks.Add(innerSource.TransformToStackStruct(i, j));
-                    //handle items in stack
-                    foreach (WorkStackItem innerInnerSource in innerSource)
-                    {
-                        tempItems.Add(innerInnerSource.TransformToItemStruct(i, j, k));
-                        //handle activetimes in item
-                        foreach (ActiveTimeSpan innerInnerInnerSource in innerInnerSource.PeriodsWhenActivated)
-                        {
-                            tempTiems.Add(innerInnerInnerSource.TransformToActiveTime(i, j, k, l));
-                            //we hit rock bottom
-                            l++;
-                        }
-                        k++;
-                    }
-                    //handle completed items in hte stack
-                    foreach (WorkStackItem innerInnerSource in innerSource.CompletedItems)
-                    {
-                        tempItems.Add(innerInnerSource.TransformToItemStruct(i, j, k));
-                        //handle activetimes in item
-                        foreach (ActiveTimeSpan innerInnerInnerSource in innerInnerSource.PeriodsWhenActivated)
-                        {
-                            tempTiems.Add(innerInnerInnerSource.TransformToActiveTime(i, j, k, l));
-                            //we hit rock bottom.. again
-                            l++;
-                        }
-                        k++;
-                    }
-                    //handle activetimespans in stack, these will get -1 as their itemId
-                    foreach (ActiveTimeSpan innerInnerSource in innerSource.PeriodsWhenActivated)
-                    {
-                        tempTiems.Add(innerInnerSource.TransformToActiveTime(i, j, -1, l));
-                        l++;
-                    }
-                    j++;
-                }
-                i++;
-            }
+            //this function calls other functions that will init the rest of the collections
+            CreateSessions(tempSeshs, tempStacks, tempItems, tempTiems);
             //resize and commit to collections
             _Sessions = tempSeshs.ToArray();
             _Stacks = tempStacks.ToArray();
             _Items = tempItems.ToArray();
             _Times = tempTiems.ToArray();
+            IsInitialized = true;
+            IsInitializing = false;
         }
+        #endregion
+
+        #region Initializing methods
+        private void CreateSessions(List<Session> tempSeshs, List<Stack> tempStacks, List<Item> tempItems, List<ActiveTime> tempTiems)
+        {
+            int i = 0, j = 0, k = 0, l = 0; //some iterators ima need
+            foreach (var source in SourceData)
+            {
+                tempSeshs.Add(source.TransformToSessionStruct(i));
+                //handle stacks in session
+                CreateStacks(tempStacks, tempItems, tempTiems, i, ref j, ref k, ref l, source.Session);
+                //handle completed stacks
+                CreateStacks(tempStacks, tempItems, tempTiems, i, ref j, ref k, ref l, source.CompletedStacks);
+                i++;
+            }
+        }
+
+        private static void CreateStacks(List<Stack> tempStacks, List<Item> tempItems, List<ActiveTime> tempTiems, int i, ref int j, ref int k, ref int l, IEnumerable<WorkStack> source)
+        {
+            foreach (WorkStack innerSource in source)
+            {
+                tempStacks.Add(innerSource.TransformToStackStruct(i, j));
+                //handle items in stack
+                CreateItems(tempItems, tempTiems, i, j, ref k, ref l, innerSource);
+                //handle completed items in hte stack
+                CreateItems(tempItems, tempTiems, i, j, ref k, ref l, innerSource.CompletedItems);
+                //handle activetimespans in stack, these will get -1 as their itemId
+                CreateActiveTimes(tempTiems, i, j, -1, ref l, innerSource.PeriodsWhenActivated);
+                j++;
+            }
+        }
+
+
+
+        private static void CreateItems(List<Item> tempItems, List<ActiveTime> tempTiems, int i, int j, ref int k, ref int l, IEnumerable<WorkStackItem> source)
+        {
+            foreach (WorkStackItem innerSource in source)
+            {
+                tempItems.Add(innerSource.TransformToItemStruct(i, j, k));
+                //handle activetimes in item
+                CreateActiveTimes(tempTiems, i, j, k, ref l, innerSource.PeriodsWhenActivated);
+                k++;
+            }
+        }
+        private static void CreateActiveTimes(List<ActiveTime> tempTiems, int i, int j, int k, ref int l, IEnumerable<ActiveTimeSpan> source)
+        {
+            foreach (ActiveTimeSpan innerInnerSource in source)
+            {
+                tempTiems.Add(innerInnerSource.TransformToActiveTime(i, j, -1, l));
+                l++;
+            }
+        }
+
+        #endregion
 
         #region Getters
         private IEnumerable<Session> TryGetSessions()
