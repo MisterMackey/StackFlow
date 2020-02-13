33fe8ff23b741c7d1493c6067e90af47776c86e7
Jasper Ris <jasper.ris@nl.abnamro.com>
2020-02-13 16:57:07 +0100

    some half finished work on the first distribution

--- StackFlow/Statistics/PriorityDistributionOverTime.cs
+++ StackFlow/Statistics/PriorityDistributionOverTime.cs
@@ -1,17 +1,34 @@
 ﻿using StackFlow.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace StackFlow.Statistics
 {
     public class PriorityDistributionOverTime
     {
+        private IStackFlowDataCollections Collection;
+        public PriorityDistributionOverTime(IEnumerable<StackFlowSession> source)
+        {
+            Collection = StackFlowDataCollections.CreateStackFlowDataCollection(source);
+            Collection.Initialize();
+        }
 
         public TimeSpan GetTimeWorkedOnPriority(WorkStackItemPriority Priority, DateTimeOffset From, DateTimeOffset Until)
         {
             TimeSpan timeSpan = new TimeSpan(ticks:0);
+            int[] StackIds = Collection.Stacks.
+                Where(stack => stack.Priority == Priority).
+                Select(s => s.Id).
+                GroupBy(id => id).
+                Select(grouping => grouping.Key).
+                ToArray();
 
+            ActiveTimeSpan sumSpan = Collection.Times.
+                Where(time => StackIds.Contains(time.StackId)).
+                Select(time => time.ActiveTimeSpan).
+                Sum();
             return timeSpan;
         }
     }
