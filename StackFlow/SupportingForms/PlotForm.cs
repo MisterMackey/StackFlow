a93b02691786fe3f2f890124016d5c20f15cbd49
Jasper Ris <jasperris93@gmail.com>
2020-02-21 20:18:19 +0100

    stuff

--- StackFlow/SupportingForms/PlotForm.cs
+++ StackFlow/SupportingForms/PlotForm.cs
@@ -17,9 +17,27 @@ namespace StackFlow.SupportingForms
         {
             InitializeComponent();
             plot = new PlotView();
+            Controls.Add(plot);
+            SuspendLayout();
             plot.Model = model;
             plot.Text = "Priority over time";
+            plot.Visible = true;
+            plot.Size = new Size(500,500);
+            plot.Location = new Point(0, 0);
             plot.Show();
+            PerformLayout();
         }
     }
 }
+/*
+ *             InitializeComponent();
+            PlotView = new PlotView();
+            this.Controls.Add(PlotView);
+            SuspendLayout();
+            PlotView.Model = PriorityDistribution.GetTestPlotModel();
+            PlotView.Visible = true;
+            PlotView.Location = new Point(0,0);
+            PlotView.Size = new Size(500, 500);
+            PlotView.Show();
+            PerformLayout();
+*/
